e752fe5776c9483736482f548a62e0b846a2ea86
James Fristachi <fristachi@aol.com>
2021-05-15 20:35:01 -0400

    heroku changes

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,10 @@ const path = require("path");
 require("dotenv").config();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
